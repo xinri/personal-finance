6a4b8cd3d682514c0a77d8c9711b06f81c0d21ab
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2019-05-17 14:25:27 +0200

    feat(FinanceDate): better display of dates

--- src/components/FinanceDate/FinanceDate.tsx
+++ src/components/FinanceDate/FinanceDate.tsx
@@ -5,6 +5,8 @@ interface Props {
 }
 
 export const FinanceDate: React.StatelessComponent<Props> = ({ date }: Props) => {
-  const dateText: string = date.toDateString();
-  return <span className="date">{dateText}</span>;
+  const year: number = date.getFullYear();
+  const month: number = date.getMonth() + 1;
+  const day: number = date.getDate();
+  return <span className="date">{`${year}-${month}-${day}`}</span>;
 };
